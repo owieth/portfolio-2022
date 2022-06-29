3da055658c22bf7fcdd153550c8e1056f26e1a9e
owieth <60756360+owieth@users.noreply.github.com>
2022-06-29 21:47:24 +0200

    feat(logo): add animated logo v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
-import { Container, Grid, Image } from '@nextui-org/react';
+import { Container, Grid, Image, useTheme } from '@nextui-org/react';
 import { NextPage } from 'next';
 import { Content } from '../components/Content';
 import { Links } from '../components/Links';
 import { Navigation } from '../components/Navigation';
 
 const Home: NextPage = () => {
+  const { type } = useTheme();
+  
   return (
     <Container gap={10} style={{ height: '100vh' }}>
       <Grid.Container gap={2} justify="space-between" alignItems="center" css={{ height: 'max-content' }}>
-        <Grid><Image src="/assets/logo.png" height={100} width={100} alt={''} /></Grid>
+        <Grid>
+          <svg viewBox="0 0 1000 200" style={{ width: '200px', height: '200px'}}>
+            <defs>
+              <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle">
+                <animateTransform
+                  attributeName="transform"
+                  begin="0s"
+                  dur="30s"
+                  type="rotate"
+                  from="0 250 250"
+                  to="360 250 250"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </defs>
+            <text dy="70" textLength="1220">
+              <textPath xlinkHref="#textcircle" style={{
+                fontSize: '48px',
+                fontFamily:  'EXCRATCH-Thin',
+                fill: type == 'dark' ? 'white' : 'black',               
+              }}>Olivier • Winkler •</textPath>
+            </text>
+          </svg>
+        </Grid>
         <Grid><Navigation /></Grid>
       </Grid.Container>
 
