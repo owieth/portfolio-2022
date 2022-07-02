328d55462a10737e018f53f116cf27e530763b5d
owieth <60756360+owieth@users.noreply.github.com>
2022-07-02 13:47:02 +0200

    feat(images): use local images instead

--- constants/images.ts
+++ constants/images.ts
@@ -1,8 +1,8 @@
 export const Images = [
-    "https://github.com/owieth/portfolio/blob/a95c9b1bbc8a741a2dfadb0a12097c54e756b86d/public/assets/images/1.jpg?raw=true",
-    "https://github.com/owieth/portfolio/blob/a95c9b1bbc8a741a2dfadb0a12097c54e756b86d/public/assets/images/2.jpg?raw=true",
-    "https://github.com/owieth/portfolio/blob/a95c9b1bbc8a741a2dfadb0a12097c54e756b86d/public/assets/images/3.jpg?raw=true",
-    "https://github.com/owieth/portfolio/blob/a95c9b1bbc8a741a2dfadb0a12097c54e756b86d/public/assets/images/4.jpg?raw=true",
-    "https://github.com/owieth/portfolio/blob/a95c9b1bbc8a741a2dfadb0a12097c54e756b86d/public/assets/images/5.jpg?raw=true",
-    "https://github.com/owieth/portfolio/blob/a95c9b1bbc8a741a2dfadb0a12097c54e756b86d/public/assets/images/6.jpg?raw=true",
-];
+    "/assets/images/1.jpg",
+    "/assets/images/2.jpg",
+    "/assets/images/3.jpg",
+    "/assets/images/4.jpg",
+    "/assets/images/5.jpg",
+    "/assets/images/6.jpg",
+]
